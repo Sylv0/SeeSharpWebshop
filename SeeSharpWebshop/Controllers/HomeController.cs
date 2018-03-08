a3b4072478617f1a517a5fded9ef7b163fefe422
Johan Sylvan <gtc124@gmail.com>
2018-03-08 15:30:57 +0100

    Started loading and showing products from db

--- SeeSharpWebshop/Controllers/HomeController.cs
+++ SeeSharpWebshop/Controllers/HomeController.cs
@@ -3,16 +3,34 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
 using SeeSharpWebshop.Models;
 
 namespace SeeSharpWebshop.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly string connectionString;
+
+        List<ProductViewModel> products = new List<ProductViewModel>();
+
+        public HomeController(IConfiguration configuration)
+        {
+            this.connectionString = configuration.GetConnectionString("ConnectionString");
+            using (var connection = new SqliteConnection(this.connectionString))
+            {
+                this.products = connection.Query<ProductViewModel>("SELECT * FROM products").ToList();
+            }
+        }
+
         public IActionResult Index()
         {
-            return View();
+
+            return View(products);
         }
 
         public IActionResult Error()
