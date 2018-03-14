4eb6e4b2e7320fdcf201ec61af5d81678fef2c5d
Johan Sylvan <gtc124@gmail.com>
2018-03-14 15:29:59 +0100

    Started adding admin page

---
+++ SeeSharpWebshop/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SeeSharpWebshop.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (Request.Cookies["adminLogged"] != "true") return RedirectToAction("Login");
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
