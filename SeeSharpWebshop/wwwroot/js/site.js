a1db3782c76391956b4012025a785bc3543e1b75
Johan Sylvan <gtc124@gmail.com>
2018-03-14 15:32:55 +0100

    Ask before clearing cart

--- SeeSharpWebshop/wwwroot/js/site.js
+++ SeeSharpWebshop/wwwroot/js/site.js
@@ -1,1 +1,6 @@
 ﻿// Write your JavaScript code.
+window.onload = () => {
+    document.querySelector("a.text-danger").addEventListener('click', e => {
+        if (!window.confirm("Are you sure?")) e.preventDefault();
+    });
+}
