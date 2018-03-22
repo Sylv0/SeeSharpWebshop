aa5ab0d26d3cf28e6c3e248f2841b1bcbf96f18d
Johan Sylvan <gtc124@gmail.com>
2018-03-22 15:31:25 +0100

    Should've commited this earlier I suppose

---
+++ SeeSharpWebshop.Project.Core/Models/ReceiptModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeeSharpWebshop.Project.Core.Models
+{
+    public class ReceiptModel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Zipcode { get; set; }
+        public string City { get; set; }
+    }
+}
