563c05e18cd2434f55c70aa5b11f696b63a14eec
Johan Sylvan <gtc124@gmail.com>
2018-03-26 14:38:48 +0200

    Started adding an API

---
+++ SeeSharpWebshop.API/Controllers/CartController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SeeSharpWebshop.Project.Core.Repositories.Implementations;
+using SeeSharpWebshop.Project.Core.Services.Implementations;
+using Newtonsoft.Json;
+using SeeSharpWebshop.Project.Core.Models;
+using System.Net;
+
+namespace SeeSharpWebshop.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class CartController : Controller
+    {
+        private readonly string connectionString;
+        private readonly CartService cartService;
+
+        public CartController(IConfiguration config)
+        {
+            this.connectionString = config.GetConnectionString("ConnectionString");
+            this.cartService = new CartService(new CartRepository(this.connectionString));
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<CartModel> Get()
+        {
+            return cartService.Get();
+        }
+
+        // GET api/values/5
+        [HttpGet("{guid}")]
+        public IEnumerable<CartModel> Get(string guid)
+        {
+            List<CartModel> list = new List<CartModel>();
+            
+            return cartService.Get(guid);
+        }
+
+        // POST api/values
+        [HttpPost("{value}")]
+        public string Post([FromBody]ProductModel value)
+        {
+            return $"{value.Name}: {value.Price}\n{value.Description} | id:{value.Id}";
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SeeSharpWebshop.API/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using SeeSharpWebshop.Project.Core.Models;
+using SeeSharpWebshop.Project.Core.Repositories.Implementations;
+using SeeSharpWebshop.Project.Core.Services.Implementations;
+
+namespace SeeSharpWebshop.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Product")]
+    public class ProductController : Controller
+    {
+        private readonly string connectionString;
+        private readonly ProductService productService;
+
+        public ProductController(IConfiguration config)
+        {
+            this.connectionString = config.GetConnectionString("ConnectionString");
+            productService = new ProductService(new ProductRepository(this.connectionString));
+        }
+        
+        [HttpGet]
+        public IEnumerable<ProductModel> Get()
+        {
+            return productService.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<ProductModel> Get(int id)
+        {
+            var list = new List<ProductModel>();
+            list.Add(productService.Get(id));
+            return list;
+        }
+    }
+}
