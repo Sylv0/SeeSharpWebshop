901edfc95d0ac4ddfa4fca6e766535da2896b873
Johan Sylvan <gtc124@gmail.com>
2018-03-29 14:08:57 +0200

    Added functions to get all carts and update cartitem

--- SeeSharpWebshop.Project.Core/Repositories/Implementations/CartRepository.cs
+++ SeeSharpWebshop.Project.Core/Repositories/Implementations/CartRepository.cs
@@ -17,6 +17,15 @@ namespace SeeSharpWebshop.Project.Core.Repositories.Implementations
             this.connectionString = connectionString;
         }
 
+        public List<CartModel> Get()
+        {
+            using (var connection = new SqliteConnection(this.connectionString))
+            {
+                var query = "select products.id as ProductID, products.name as Name, products.price as Price, carts.Amount FROM products, carts WHERE carts.ProductID=products.id";
+                return connection.Query<CartModel>(query).ToList();
+            }
+        }
+
         public List<CartModel> Get(string guid)
         {
             using (var connection = new SqliteConnection(this.connectionString))
@@ -24,6 +33,20 @@ namespace SeeSharpWebshop.Project.Core.Repositories.Implementations
                 var query = "select products.id as ProductID, products.name as Name, products.price as Price, carts.Amount FROM products, carts WHERE carts.guid=@guid AND carts.ProductID=products.id";
                 return connection.Query<CartModel>(query, new { guid }).ToList();
             }
+        }
+
+        public bool UpdateCart(string guid, int Id, int Amount)
+        {
+            using (var connection = new SqliteConnection(this.connectionString))
+            {
+                try
+                {
+                    var query = "UPDATE carts SET Amount=@Amount WHERE guid=@guid AND ProductID=@Id";
+                    var test = connection.Execute(query, new { Amount, guid, Id });
+                }
+                catch (Exception e) { return false; }
+            }
+            return true;
         }
 
         public void Clear(string guid)
