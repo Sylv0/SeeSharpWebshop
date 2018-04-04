04628733541cd0530268529ffaaf18fe07df81f3
Johan Sylvan <gtc124@gmail.com>
2018-04-04 08:47:42 +0200

    Started looking into integration tests

---
+++ SeeSharpWebshop.IntegrationTests/Repositories/CartRepositoryTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using SeeSharpWebshop.Project.Core.Repositories.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeeSharpWebshop.IntegrationTests.Repositories
+{
+    public class CartRepositoryTests
+    {
+        private CartRepository cartRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.cartRepository = new CartRepository("Data Source=../../SeeSharpWebshop/App_Data/shop.db");
+        }
+
+        [Test]
+        public void Get_GivenGuid_ReturnsExpectedCart()
+        {
+            const string guid = "2cf43b90-46f9-4b0f-9d85-e7b4f4b688d1";
+
+            var result = cartRepository.Get(guid);
+        }
+    }
+}
