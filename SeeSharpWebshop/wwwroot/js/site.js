d46e5eb682d018d4c3ac67784e40b4a0be352d44
Johan Sylvan <gtc124@gmail.com>
2018-04-04 08:46:42 +0200

    Moved Javasccript to file and added some more

--- SeeSharpWebshop/wwwroot/js/site.js
+++ SeeSharpWebshop/wwwroot/js/site.js
@@ -1,6 +1,45 @@
 ﻿// Write your JavaScript code.
+const updateCart = (cookie) => {
+    fetch(`http://localhost:51421/api/cart/${cookie}`)
+        .then(res =>
+            res.json())
+        .then(data =>
+            document.querySelector('span.badge').innerHTML = data);
+}
 window.onload = () => {
-    document.querySelector("a.text-danger").addEventListener('click', e => {
-        if (!window.confirm("Are you sure?")) e.preventDefault();
+    const cookie = document.cookie.match(/guid=([a-zA-Z0-9-]*)/)[1];
+    try {
+        document.querySelector("a.text-danger").addEventListener('click', e => {
+            if (!window.confirm("Are you sure?")) e.preventDefault();
+        });
+    } catch (e) { }
+    console.log(document.cookie.match(/guid=([a-zA-Z0-9-]*)/));
+
+    document.querySelectorAll("input.amount").forEach(e => {
+        e.addEventListener("change", e => {
+            if (e.target.value <= 0) {
+                if (!window.confirm("You're settng the value to less than 1, would you like to romove it?"))
+                    e.target.value = 1;
+                else
+                    return;
+            }
+
+            let data = new FormData();
+            data.append('guid', cookie);
+            data.append('Id', e.target.dataset['product']);
+            data.append('Amount', e.target.value);
+            let request = new Request("http://localhost:51421/api/cart/UpdateCart", {
+                method: "POST", body: data
+            });
+
+            fetch(request)
+                .then(a => a.json())
+                .then(b => {
+                    if (b == "200") console.log("IT WORKS!");
+                    updateCart(cookie);
+                })
+                .catch(e => console.log(e));
+        });
     });
+    updateCart(cookie);
 }
