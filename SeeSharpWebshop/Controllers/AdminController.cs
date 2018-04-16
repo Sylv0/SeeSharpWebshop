00464af0d11287724b204d13c89b4f6f2a2f134a
Johan Sylvan <gtc124@gmail.com>
2018-04-16 08:55:24 +0200

    Added admin login

--- SeeSharpWebshop/Controllers/AdminController.cs
+++ SeeSharpWebshop/Controllers/AdminController.cs
@@ -6,6 +6,13 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SeeSharpWebshop.Controllers
 {
+
+    public class LoginModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
     public class AdminController : Controller
     {
         public IActionResult Index()
@@ -14,9 +21,31 @@ namespace SeeSharpWebshop.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult Login()
         {
+            if (!string.IsNullOrEmpty(Request.Cookies["adminLogged"]))
+            {
+                return RedirectToAction("Logout");
+            }
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if (model.Email == "a@a.a" && model.Password == "admin")
+            {
+                Response.Cookies.Append("adminLogged", "true");
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            Response.Cookies.Delete("adminLogged");
+            return RedirectToAction("Login");
         }
     }
 }
