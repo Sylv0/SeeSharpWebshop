adb5b7a7c6723763ab0e8942b7153b55c756dfce
Johan Sylvan <gtc124@gmail.com>
2018-04-18 17:25:39 +0200

    JS and CSS

--- SeeSharpWebshop/wwwroot/js/site.js
+++ SeeSharpWebshop/wwwroot/js/site.js
@@ -6,13 +6,38 @@ const updateCart = (cookie) => {
         .then(data =>
             document.querySelector('span.badge').innerHTML = data);
 }
+
+const AddLatestProductToList = (id, n, d, p) => {
+    const target = document.querySelector(".row");
+
+    fetch("http://localhost:51421/api/product").then(res => res.json()).then(data => {
+        if (data.length > 0) {
+            const post = data[data.length - 1];
+            console.log(post);
+            target.innerHTML += `
+                <div class="col-md-4 col-sm-12 product-panel">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">${post.name}</h3>
+                        </div>
+                        <div class="panel-body">
+                            ${post.description}
+                        </div>
+                        <div class="panel-footer">${post.id} kr<a href="Home/AddItemToCart/${post.id}" class="pull-right add-to-cart"><span class="glyphicon glyphicon-plus"></span></a></div>
+                    </div>
+                </div>
+            `;
+        }
+    });
+}
+
 window.onload = () => {
     const cookie = document.cookie.match(/guid=([a-zA-Z0-9-]*)/)[1];
     try {
         document.querySelector("a.text-danger").addEventListener('click', e => {
             if (!window.confirm("Are you sure?")) e.preventDefault();
         });
-    } catch (e) { console.log(e); }
+    } catch (e) { }
 
     try {
         document.querySelectorAll("input.amount").forEach(e => {
@@ -54,7 +79,7 @@ window.onload = () => {
                     .catch(e => console.log(e));
             });
         });
-    } catch (e) { console.log(e); }
+    } catch (e) { }
 
     try {
         document.querySelectorAll("a.add-to-cart").forEach(e => {
@@ -77,8 +102,30 @@ window.onload = () => {
                     .catch(e => console.log(e));
             });
         });
-    } catch (e) {
-        console.error(e);
-    }
+    } catch (e) { }
+
+    try {
+        document.forms['add_product_form'].addEventListener('submit', e => {
+            e.preventDefault();
+            let data = new FormData();
+            data.append('Name', e.target.name.value);
+            data.append('Description', e.target.description.value);
+            data.append('Price', e.target.price.value);
+            let request = new Request("http://localhost:51421/api/product/Add", {
+                method: "POST", body: data
+            });
+
+            fetch(request)
+                .then(a => a.json())
+                .then(b => {
+                    if (b === 200) {
+                        $('#myModal').modal('hide');
+                        AddLatestProductToList();
+                    }
+                })
+                .catch(e => console.log(e));
+        });
+    } catch (e) { }
+
     updateCart(cookie);
 }
